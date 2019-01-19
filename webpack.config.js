cc1b8426af696550b6f20fa0bf374c5c6674721d
cw <userchenwei@gmail.com>
2019-01-19 14:10:12 +0800

    init

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin'); //installed via npm
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+//const webpack = require('webpack');
+
+module.exports = {
+  entry: {
+    app: path.resolve(__dirname, 'src') + '/index.js',
+    print: path.resolve(__dirname, 'src') + '/print.js'
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].bundle.js'
+  },
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000,
+    disableHostCheck: true,
+    host: '0.0.0.0'
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      title: 'Output Management'
+    })
+
+  ]
+};
